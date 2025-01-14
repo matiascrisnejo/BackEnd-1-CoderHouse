18805a6c99e62bf4e005af83d88b8dbef71f8b4d
matiascrisnejo <matias.crisnejo@estudiantes.unahur.edu.ar>
2025-01-14 14:41:09 -0300

    correcion 2
    correccion

--- src/app.js
+++ src/app.js
@@ -1,8 +1,4 @@
 import express from 'express';
-<<<<<<< Updated upstream
-import productsRoutes from './routes/products.routes.js';
-import cartsRoutes from './routes/carts.routes.js';
-=======
 
 import {ProductManager} from './ProductManager.js';
 import {CartManager} from "./CartManager.js"
@@ -10,22 +6,7 @@ import {CartManager} from "./CartManager.js"
 import routerCarts from "./routes/carts.router.js"
 import routerProducts from "./routes/products.router.js"
 
->>>>>>> Stashed changes
-
-
-const PORT = 8080;
 const app = express();
-<<<<<<< Updated upstream
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use('/api/products', productsRoutes);
-app.use('/api/carts', cartsRoutes);
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-=======
 const port = 8080;
 
 app.use(express.json())
@@ -42,4 +23,3 @@ app.use('/api/carts', routerCarts);
 
 
 app.listen(port, () => console.log("servidor con express"))
->>>>>>> Stashed changes
